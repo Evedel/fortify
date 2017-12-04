9c1bc581c2b3244ce0547bcab8f86703cf06a53f
Sergei Biriukov <svbiriukov@gmail.com>
2017-12-04 14:23:08 +1100

    Open file + parse name + compile hello world in tex

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"os"
 	"fmt"
 	"flag"
+	"strings"
 	"io/ioutil"
 
 	"compile"
@@ -14,12 +16,58 @@ func main() {
 	tailInput := flag.Args()
 	fmt.Println("-> Input File: ", tailInput)
 	if len(tailInput) == 1 {
-		if b, err := ioutil.ReadFile(tailInput[0]); err != nil {
-				fmt.Print(err)
+		sourceInputFull := tailInput[0]
+		sourceName := [3]string{ sourceInputFull,
+														sourceInputFull,
+														"" }
+		idot := strings.LastIndex(sourceInputFull, ".")
+		isls := strings.LastIndex(sourceInputFull, "/")
+		if idot != -1 {
+			if isls != -1 {
+				if isls < idot {
+					// adfasdfasf/asfaf.asdf
+					sourceName[0] = sourceInputFull[isls+1:idot]
+					sourceName[1] = sourceInputFull[isls+1:]
+					sourceName[2] = sourceInputFull[:isls]
+				} else {
+					// sdfasdfa.sdfasdf/asdfadsf
+					sourceName[0] = sourceInputFull[isls+1:]
+					sourceName[1] = sourceInputFull[isls+1:]
+					sourceName[2] = sourceInputFull[:isls]
+				}
+			} else {
+				// adsfaf.adfad
+				sourceName[0] = sourceInputFull[:idot]
+				sourceName[1] = sourceInputFull
+				sourceName[2] = ""
+			}
 		} else {
-			source = source + string(b)
+			if isls != -1 {
+				// adfadsf/adfadf
+				sourceName[0] = sourceInputFull[isls+1:]
+				sourceName[1] = sourceInputFull[isls+1:]
+				sourceName[2] = sourceInputFull[:isls]
+			} else {
+				// adfasdfads
+        // default from definition
+			}
 		}
-		compile.LaTeX(source, tailInput[0])
+		fmt.Print("-> Working Directory: ", sourceName, "\n")
+		if _, err := os.Stat(sourceInputFull); os.IsNotExist(err) {
+			fmt.Printf("File %s doesn't exist.\n", sourceName)
+		} else {
+			err := os.Mkdir(sourceName[2] + "/" + sourceName[0], 0744)
+			if (err == nil) || (os.IsExist(err)) {
+				if b, err := ioutil.ReadFile(sourceInputFull); err != nil {
+						fmt.Print(err)
+				} else {
+					source = source + string(b)
+				}
+				compile.LaTeX(source, sourceName)
+			} else {
+				fmt.Print("-> Error: ", err, "\n")
+			}
+		}
 	}
 	fmt.Printf("-> Done\n")
 }
---
+++ src/compile/LaTeX.go
@@ -0,0 +1,59 @@
+package compile
+
+import (
+  "fmt"
+  "os/exec"
+  "strings"
+  "io/ioutil"
+)
+
+var Mirrorred = []string{"#"}
+var UnMirrorred = []string{"\\say"}
+
+func LaTeX(source string, name [3]string) {
+  strsc := source
+  strnm := name
+  strscnew := ""
+  fmt.Print("-> LaTeX compile: ", strnm[0], "\n")
+  for _, line := range strings.Split(strsc, "\n") {
+    newline := line
+    idlc := strings.Index(line, "!")
+    if idlc != -1 {
+      newline = line[:idlc]
+    }
+    if idlc != 0 {
+      strscnew += newline + "\n"
+    }
+  }
+  strsc = strscnew
+  strsc = strings.Replace(strsc, "\n", " \\\\ \n & ", -1)
+  for _, ch := range Mirrorred {
+    strsc = strings.Replace(strsc, ch, "\\" + string(ch), -1)
+  }
+  for _, ch := range UnMirrorred {
+    strsc = strings.Replace(strsc, ch, ch[1:], -1)
+  }
+  strsc = "& " + strsc + "\n"
+  strsc = "\\documentclass[12pt]{article} \n" +
+          "\\usepackage{amsmath} \n" +
+          "\\begin{document} \n" +
+          "\\begin{equation} \n" +
+          "\\begin{aligned} \n" +
+          strsc +
+          "\\end{aligned} \n" +
+          "\\end{equation} \n" +
+          "\\end{document} \n"
+
+  if err := ioutil.WriteFile("./" + strnm[2] + "/" + strnm[0] + "/" + strnm[0] + ".tex",
+    []byte(strsc), 0644); err != nil {
+      fmt.Print("-> Error: ", err, "\n")
+  } else {
+    cmd := exec.Command("pdflatex", "-output-directory", "./" + strnm[2] + "/" + strnm[0] + "/",
+      "./" + strnm[2] + "/" + strnm[0] + "/" + strnm[0] + ".tex")
+    if output, err := cmd.Output(); err != nil {
+      fmt.Print("-> Error: ", err, "\n")
+      fmt.Print("-> Error: ", cmd.Args, "\n")
+      fmt.Print("-> Error: ", string(output), "\n")
+    }
+  }
+}
